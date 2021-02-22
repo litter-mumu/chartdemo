77564970a40c6de6c4fc6b7dd0e0aa69fd01557c
lpf <1198150498@qq.com>
2021-02-22 18:52:54 +0800

    动态显示数据30个，并设置与右边坐标轴的距离

--- app/src/main/java/com/cash/hunterchartdemo/LineChartActivity1.java
+++ app/src/main/java/com/cash/hunterchartdemo/LineChartActivity1.java
@@ -25,9 +25,7 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.formatter.IFillFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.Utils;
 import com.google.gson.Gson;
@@ -173,16 +171,10 @@ public class LineChartActivity1 extends DemoBase {
     private void initChart() {
         {   // // Chart Style // //
             chart = findViewById(R.id.chart1);
-
-            // background color
-            chart.setBackgroundColor(Color.parseColor("#2B3549"));
-
-            // disable description text
+            chart.setBackgroundColor(Color.parseColor("#242E4B"));
             chart.getDescription().setEnabled(false);
-
             // enable touch gestures
             chart.setTouchEnabled(false);
-
             chart.setDrawGridBackground(false);
 
             MyMarkerView2 mv = new MyMarkerView2(this, R.layout.custom_marker_view2);
@@ -193,19 +185,18 @@ public class LineChartActivity1 extends DemoBase {
 
             // force pinch zoom along both axis
             chart.setPinchZoom(true);
-
             chart.setVisibleXRangeMaximum(30);
         }
 
         XAxis xAxis;
         {   // // X-Axis Style // //
             xAxis = chart.getXAxis();
-
             xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-
+            xAxis.setTextColor(Color.parseColor("#656E87"));
             xAxis.setValueFormatter(new IAxisValueFormatter() {
                 @Override
                 public String getFormattedValue(float value, AxisBase axis) {
+                    //两秒一次，因此 *2
                     long timestamp = ((long) (value * 2) + currentTime) * 1000;
 //                    Log.e("LineChartActivity1", "long = " + value);
                     return Util.getDateToString(timestamp);
@@ -213,48 +204,22 @@ public class LineChartActivity1 extends DemoBase {
             });
         }
 
-        YAxis yAxis;
+        YAxis leftAxis, rightAxis;
         {   // // Y-Axis Style // //
-            yAxis = chart.getAxisLeft();
+            leftAxis = chart.getAxisLeft();
+            leftAxis.setEnabled(false);
 
-            // disable dual axis (only use LEFT axis)
-//            chart.getAxisLeft().setEnabled(false);
-
-            // horizontal grid lines
-//            yAxis.enableGridDashedLine(10f, 10f, 0f);
-
-            // axis range
-//            yAxis.setAxisMaximum(200f);
-//            yAxis.setAxisMinimum(-50f);
+            rightAxis = chart.getAxisRight();
+            rightAxis.setTextColor(Color.parseColor("#AEB5C7"));
         }
 
-        setData();
-
-        {   // // Create Limit Lines // //
-//            LimitLine ll2 = new LimitLine(values.get(values.size() - 1).getY(), String.valueOf(values.get(values.size() - 1).getY()));
-//            ll2.setLineWidth(1.5f);
-//            ll2.setLineColor(Color.WHITE);
-//            ll2.setLabelPosition(LimitLabelPosition.RIGHT_BOTTOM);
-//            ll2.setTextSize(10f);
-//            ll2.setTextColor(Color.WHITE);
-//            ll2.setTypeface(tfRegular);
-//
-//            // draw limit lines behind data instead of on top
-//            yAxis.setDrawLimitLinesBehindData(true);
-//            // add limit lines
-//            yAxis.addLimitLine(ll2);
-
-            Highlight highlight = new Highlight(values.get(values.size() - 1).getX(), 0, -1);
-            chart.highlightValue(highlight, false);
-        }
-
+        setInitData();
 
         // draw points over time
-        chart.animateX(500);
+        chart.animateX(200);
 
         // get the legend (only possible after setting data)
         Legend l = chart.getLegend();
-
         // draw legend entries as lines
         l.setForm(LegendForm.LINE);
     }
@@ -262,6 +227,9 @@ public class LineChartActivity1 extends DemoBase {
     //时间区间 默认1分钟 两秒请求一次，所以默认30
     private int timeInterval = 30;
 
+    /**
+     * TODO 这里会有一个bug，如果网速好的情况下，当前时间加上请求次数是没问题的，但是网速不好的情况下，时间会越来越小
+     */
     private void refreshChartLastData() {
         socketTime++;
 
@@ -269,25 +237,45 @@ public class LineChartActivity1 extends DemoBase {
         Entry entry = new Entry(timeInterval + socketTime, Util.getAverage(bean.getAsk(), bean.getBid()));
         values.add(entry);
 
-        set1 = (LineDataSet) chart.getData().getDataSetByIndex(0);
-        set1.setValues(values);
-        set1.notifyDataSetChanged();
-        chart.getData().notifyDataChanged();
-        chart.notifyDataSetChanged();
-        chart.invalidate();
+        Highlight highlight = new Highlight(values.get(values.size() - 1).getX(), 0, -1);
+        chart.highlightValue(highlight, false);
+
+        LineData data = chart.getData();
+        if (data != null) {
+            ILineDataSet set = data.getDataSetByIndex(0);
+
+            if (set == null) {
+                return;
+            }
+
+            data.addEntry(entry, 0);
+            data.notifyDataChanged();
+
+
+            chart.getXAxis().setAxisMaximum(timeInterval + socketTime + 5);
+
+            chart.notifyDataSetChanged();
+            chart.setVisibleXRangeMaximum(30);
+            //TODO 加动画的话，锚点不是同步进行的，再找其他办法看看
+//            chart.moveViewToAnimated(entry.getX(),entry.getY(),set.getAxisDependency(),500);
+
+            chart.moveViewToX(data.getEntryCount());
+        }
     }
 
     private long currentTime;
 
     private LineDataSet set1;
-    private void setData() {
+
+    /**
+     * 设置初始数据
+     */
+    private void setInitData() {
         currentTime = chartBeans.get(chartBeans.size() - timeInterval).getEpoch();
         for (int i = 0; i < timeInterval; i++) {
             ChartBean bean = chartBeans.get(chartBeans.size() - (timeInterval - i));
             Log.d("LineChartActivity1", i + "  " + bean.toString());
             Entry entry = new Entry(i, Util.getAverage(bean.getAsk(), bean.getBid()));
-//            Log.d("LineChartActivity1", "x = " + entry.getX() + "  y = " + Util.getAverage(bean.getAsk(), bean.getBid())
-//                    + "   " + Util.getDateToString((bean.getEpoch() * 1000)));
             values.add(entry);
         }
 
@@ -300,7 +288,6 @@ public class LineChartActivity1 extends DemoBase {
         } else {
             // create a dataset and give it a type
             set1 = new LineDataSet(values, "");
-
             set1.setDrawIcons(false);
             set1.setDrawCircles(false);
             set1.setColor(Color.parseColor("#67ABF7"));
@@ -320,18 +307,14 @@ public class LineChartActivity1 extends DemoBase {
 //            set1.setValueTextSize(9f);
             set1.setDrawValues(false);
 
+            //自定义高亮时的十字指示
             set1.setDrawVerticalHighlightIndicator(false);
+            set1.setDrawHorizontalHighlightIndicator(false);
             set1.setHighLightColor(Color.WHITE);
             set1.setHighlightLineWidth(1f);
 
             // set the filled area
             set1.setDrawFilled(true);
-            set1.setFillFormatter(new IFillFormatter() {
-                @Override
-                public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
-                    return chart.getAxisLeft().getAxisMinimum();
-                }
-            });
 
             // set color of filled area
             if (Utils.getSDKInt() >= 18) {
